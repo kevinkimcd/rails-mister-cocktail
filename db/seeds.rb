d9710b7699bd46c9559356ec46ed646ff4796ca8
Kevin Kim <khk271@nyu.edu>
2020-04-03 08:03:52 -0400

    built seed by parsing JSON of 100 ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient = JSON.parse(open(url).read)
+
+ingredient["drinks"].each do |x|
+  Ingredient.create(name: x.values)
+end
